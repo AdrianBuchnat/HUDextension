69ba6da6a26ea09bf20b85bdea4bd13d1c57a62f
Adrian Buchnat <adrianbuchnat@gmail.com>
2026-02-18 17:08:58 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using HUDextension;
+
+class Program
+{
+
+
+    static void Main()
+    {
+        IntPtr w3window = NativeMethods.FindWindow(null, "Warcraft III");
+
+        if (w3window != IntPtr.Zero)
+        {
+            if (NativeMethods.GetWindowRect(w3window, out RECT rect))
+            {
+                int width = rect.Right - rect.Left;
+                int height = rect.Bottom - rect.Top;
+                Console.WriteLine($"{width}x{height}");
+
+
+                double hp;
+                UnitFrame unitFrame = new UnitFrame(w3window, 1161, 1199, 914);
+
+                int i = 0;
+                while (true)
+                {
+                    Thread.Sleep(500);
+
+                    hp = unitFrame.GetHealthPercentage();
+                    Console.WriteLine($"hp: {hp*100:F2}%");
+
+                    if (i == 50) break;
+                    i++;
+                }
+
+
+            }
+        }
+        else
+        {
+            Console.WriteLine("Nic nie znaleziono");
+        }
+
+
+    }
+
+
+
+
+
+
+}
+
---
+++ UnitFrame.cs
@@ -0,0 +1,65 @@
+﻿namespace HUDextension
+{
+    public class UnitFrame
+    {
+        private int startHpBar, heightHpBar;
+        private double hpBarLeng;
+        private IntPtr window;
+
+
+        public UnitFrame(IntPtr window, int startHpBar, int endHpBar, int heightHpBar )
+        {
+            this.window = window;
+            this.startHpBar = startHpBar;
+            this.heightHpBar = heightHpBar;
+            this.hpBarLeng = endHpBar - startHpBar;
+        }
+
+
+
+        public double GetHealthPercentage()
+        {
+            double currentHealthPixels = this.hpBarLeng;
+            IntPtr dc = NativeMethods.GetDC(this.window);
+
+            if (dc == IntPtr.Zero) return 0.0;
+
+            try {
+                for (int i = 0; i < this.hpBarLeng; i++)
+                {
+                    int col = NativeMethods.GetPixel(dc, this.startHpBar + i, this.heightHpBar);
+                    var (r, g, b) = this.GetPixelColor(col);
+                    if (r == 0 && g == 0) currentHealthPixels--;
+                }
+
+
+                return currentHealthPixels / this.hpBarLeng;
+            }
+            finally
+            {
+                NativeMethods.ReleaseDC(this.window, dc);
+            }
+        }
+
+
+
+        private (int, int, int) GetPixelColor(int col)
+        {
+            if (col == -1)
+            {
+                Console.WriteLine("Nie udalo sie zgarnac koloru pixela");
+                return (-1, -1, -1);
+            }
+            else
+            {
+                int r = col & 0xFF;
+                int g = (col >> 8) & 0xFF;
+                int b = (col >> 16) & 0xFF;
+
+                return (r, g, b);
+            }
+        }
+
+
+    }
+}
